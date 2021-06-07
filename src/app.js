dd4d0e0eea23d97b5cf54ddfcdcc82633be93407
Love-YGX-and-D <1578236889@qq.com>
2021-06-07 14:23:33 +0800

    feat: optimize code

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DTable from 'dtable-sdk';
-import { Input, Button } from 'reactstrap'
+import { Input, Button } from 'reactstrap';
 import intl from 'react-intl-universal';
-import './locale/index.js'
+import './locale/index.js';
+import { Loading } from './components';
+import { QUERY_STATUS, PER_DISPLAY_COUNT } from './constants';
+import LOGO from './assets/images/sql-query.png';
 
-import './assets/css/plugin-layout.css';
 import './assets/css/app.css';
-
-const QUERY_STATUS = {
-  READY: 'ready',
-  DOING: 'doing',
-  DONE: 'done'
-};
 
 const propTypes = {
   showDialog: PropTypes.bool,
@@ -28,8 +24,12 @@ class App extends React.Component {
       sql: '',
       result: {},
       queryStatus: QUERY_STATUS.READY,
+      displayResultsCount: PER_DISPLAY_COUNT,
+      isLoading: false,
     };
     this.dtable = new DTable();
+    this.sqlQueryResultRef = null;
+    this.sqlQueryResultContentRef = null;
   }
 
   componentDidMount() {
@@ -43,6 +43,8 @@ class App extends React.Component {
   componentWillUnmount() {
     this.unsubscribeLocalDtableChanged();
     this.unsubscribeRemoteDtableChanged();
+    this.sqlQueryResultRef = null;
+    this.sqlQueryResultContentRef = null;
   }
 
   async initPluginDTableData() {
@@ -89,16 +91,30 @@ class App extends React.Component {
       this.setState({queryStatus: QUERY_STATUS.DOING}, () => {
         const { dtableAPI } = window.app.dtableStore;
         dtableAPI.sqlQuery(sql, 'dtable-server').then(res => {
-          this.setState({queryStatus: QUERY_STATUS.DONE, result: res.data});
+          this.setState({ queryStatus: QUERY_STATUS.DONE, result: res.data });
         }).catch(e => {
-          this.setState({queryStatus: QUERY_STATUS.DONE, result: e});
+          this.setState({ queryStatus: QUERY_STATUS.DONE, result: e });
         });
       });
     }
   }
 
+  getMoreResults = () => {
+    const { isLoading, displayResultsCount, queryStatus, result } = this.state;
+    if (isLoading) return;
+    if (queryStatus !== QUERY_STATUS.DONE) return;
+    const { success, results } = result;
+    if (!success) return;
+    if (displayResultsCount >= results.length) return;
+    if (this.sqlQueryResultContentRef.offsetHeight + this.sqlQueryResultContentRef.scrollTop >= this.sqlQueryResultRef.offsetHeight) {
+      this.setState({ isLoading: true }, () => {
+        this.setState({ isLoading: false, displayResultsCount: displayResultsCount + PER_DISPLAY_COUNT });
+      });
+    }
+  }
+
   renderResult = () => {
-    const { result, queryStatus } = this.state;
+    const { result, queryStatus, displayResultsCount, isLoading } = this.state;
     if (queryStatus === QUERY_STATUS.READY) return (
       <div className="sql-query-result ready"></div>
     );
@@ -108,40 +124,45 @@ class App extends React.Component {
       </div>
     );
     const { success, error_message, results } = result;
+    const displayResults = results.slice(0, displayResultsCount);
     if (success) {
-      const firstResult = results[0];
+      const firstResult = displayResults[0];
       if (!firstResult) return '';
       const keys = Object.keys(firstResult);
       return (
         <div className="sql-query-result success">
-          <table className="sql-query-result-table">
-            <thead className="sql-query-result-thead">
-              <tr className="sql-query-result-thead-tr" key="-1">
-                {keys.map(key => {
+          <div className="sql-query-result-content" onScroll={this.getMoreResults} ref={ref => this.sqlQueryResultContentRef = ref}>
+            <table className="sql-query-result-table" ref={ref => this.sqlQueryResultRef = ref}>
+              <thead className="sql-query-result-thead">
+                <tr className="sql-query-result-thead-tr" key="-1">
+                  {keys.map(key => {
+                    return (
+                      <th className="sql-query-result-thead-th" key={key + '--1'}>
+                        {key}
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                {displayResults.map((result, index) => {
                   return (
-                    <th className="sql-query-result-thead-th" key={key + "--1"}>
-                      {key}
-                    </th>
+                    <tr className="sql-query-result-tbody-tr" key={index}>
+                      {keys.map(key => {
+                        const value = result[key];
+                        return (
+                          <td className="sql-query-result-tbody-td" key={`${key}-${{index}}`}>
+                            {typeof value === 'object' ? value + '' : value}
+                          </td>
+                        );
+                      })}
+                    </tr>
                   );
                 })}
-              </tr>
-            </thead>
-            <tbody>
-              {results.map((result, index) => {
-                return (
-                  <tr className="sql-query-result-tbody-tr" key={index}>
-                    {keys.map(key => {
-                      return (
-                        <td className="sql-query-result-tbody-td" key={`${key}-${{index}}`}>
-                          {result[key]}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                  );
-              })}
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+            {isLoading && <Loading />}
+          </div>
         </div>
       );
     }
@@ -157,26 +178,13 @@ class App extends React.Component {
     if (!showDialog) {
       return '';
     }
-
-    /*
-      <Modal isOpen={showDialog} toggle={this.onPluginToggle} className="dtable-plugin plugin-container sql-query-plugin-dialog" size="lg">
-        <ModalHeader className="sql-query-plugin-header" toggle={this.onPluginToggle}>{intl.get('SQL_query')}</ModalHeader>
-        <ModalBody className="sql-query-plugin-body">
-          <div className="sql-input-container">
-            <Input className="sql-input" value={sql} onChange={this.onChange}/>
-            <Button color="primary" className="query-sql-button" onClick={this.onQuery}>{intl.get('Query')}</Button>
-          </div>
-          {this.renderResult()}
-        </ModalBody>
-      </Modal>
-    
-    */
     
     return (
       <div className="dtable-plugin sql-query-plugin">
         <div className="sql-query-plugin-header">
           <div className="sql-query-plugin-header-left">
-            {intl.get('SQL_query')}
+            <img src={LOGO} alt="logo" width="24"/>
+            <span className="ml-2">{intl.get('SQL_query')}</span>
           </div>
           <div className="sql-query-plugin-header-right" onClick={this.onCloseToggle}>
             <i title={intl.get('Close')} className="dtable-font dtable-icon-fork-number"></i>
@@ -184,7 +192,7 @@ class App extends React.Component {
         </div>
         <div className="sql-query-plugin-body">
           <div className="sql-input-container">
-            <Input className="sql-input" value={sql} onChange={this.onChange}/>
+            <Input className="sql-input" value={sql} onChange={this.onChange} autoFocus={!sql} />
             <Button
               color="primary"
               className="query-sql-button"
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const QUERY_STATUS = {
+  READY: 'ready',
+  DOING: 'doing',
+  DONE: 'done'
+};
+
+export const PER_DISPLAY_COUNT = 25;
