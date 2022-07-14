bf24c2acfd884c7a2c9fccffd2fcef0c33ea9c89
er-pai-r <18335219360@163.com>
2022-07-14 12:12:55 +0800

    fix: not select bug

--- src/pages/body/index.js
+++ src/pages/body/index.js
@@ -74,6 +74,8 @@ class Body extends Component {
 
   getValidSQL = (sql) => {
     const upperSQL = sql.toUpperCase();
+    const trimUpperSQL = upperSQL.trim();
+    if (!trimUpperSQL.startsWith('SELECT ')) return sql;
     const selectIndex = upperSQL.indexOf('SELECT ');
     const fromIndex = upperSQL.indexOf(' FROM ');
     const selectedColumnsString = sql.slice(selectIndex + 7, fromIndex);
